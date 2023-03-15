fa202312676bdb146958fd112186a9fe940d103b
Yael Harosh <yaelharosh01@gmail.com>
2023-03-15 10:01:57 -0400

    finish changes

--- src/Child.js
+++ src/Child.js
@@ -1,8 +1,15 @@
 import React from "react";
 import { getRandomColor } from "./randomColorGenerator.js";
 
-function Child({ onChangeColor }) {
-  return <div className="child" style={{ backgroundColor: "#FFF" }} />;
+function Child({ onChangeColor, color }) {
+  // console.log(onChangeColor);
+
+  function handleClick() {
+    const newColor = getRandomColor();
+    onChangeColor(newColor);
+  }
+
+  return <div onClick={ handleClick } className="child" style={{ backgroundColor: color }} />;
 }
 
 export default Child;
